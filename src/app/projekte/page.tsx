42e33d8b6c52ccbf97c7b027aa9a53a8b471251c
Dennis <dennisdiepolder@outlook.com>
2024-06-10 15:47:15 +0200

    dynamodb get fixed

--- src/app/projekte/page.tsx
+++ src/app/projekte/page.tsx
@@ -15,7 +15,11 @@ interface Project {
 
 async function fetchProjects(): Promise<Project[]> {
     const client = new DynamoDBClient({
-        region: 'eu-central-1'
+        region: 'eu-central-1',
+        credentials: {
+            accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+            secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+        }
     });
 
     const dynamoDb = DynamoDBDocumentClient.from(client);
