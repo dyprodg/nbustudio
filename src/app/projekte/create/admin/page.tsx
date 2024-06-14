3407bbbd02569b7e8f5571c57623c72ae1909dbc
Dennis <dennisdiepolder@outlook.com>
2024-06-14 12:21:19 +0200

    projects admin page fetch

--- src/app/projekte/create/admin/page.tsx
+++ src/app/projekte/create/admin/page.tsx
@@ -1,18 +1,42 @@
 'use client'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { Amplify } from 'aws-amplify';
 import type { WithAuthenticatorProps } from '@aws-amplify/ui-react';
 import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
 import config from '@/amplifyconfiguration.json';
-import { post } from 'aws-amplify/api';
+import { post, get } from 'aws-amplify/api';
 import { fetchAuthSession } from '@aws-amplify/auth';
 Amplify.configure(config);
 
+interface Project {
+    title: { S: string };
+    description: { S: string };
+    link: { S: string };
+}
 
 function Page({ signOut, user }: WithAuthenticatorProps) {
     const [postTitle, setPostTitle] = useState('');
     const [postDesc, setPostDesc] = useState('');
+    const [projects, setProjects] = useState<Project[]>([]); 
+
+    async function fetchProjects() {
+        try {
+            const restOperation = get({
+                apiName: 'postsget',
+                path: '/get',
+            });
+            const response = await restOperation.response;
+            const data = await response.body.json();
+            setProjects(data as any);
+        } catch (error) {
+            console.log('GET call failed: ', JSON.parse((error as any).response.body));
+        }
+    }
+
+    useEffect(() => {
+        fetchProjects();
+    }, []);
 
     const handlePost = async () => {
         const authToken = (await fetchAuthSession()).tokens?.idToken?.toString();
@@ -78,6 +102,15 @@ function Page({ signOut, user }: WithAuthenticatorProps) {
                 >
                     Erstellen
                 </button>
+            </div>
+            <div className='w-full h-auto flex flex-col items-center space-y-2'>
+            {projects.map((project: any, index: number) => (
+                    <div key={index} className="p-4 border rounded-lg shadow-md">
+                        <h2 className="text-xl font-bold">{project.title.S}</h2>
+                        <p className="text-gray-700">{project.description.S}</p>
+                        <a href={project.link.S} className="text-blue-500 underline">Mehr erfahren</a>
+                    </div>
+                ))}
             </div>
         </div>
     );
